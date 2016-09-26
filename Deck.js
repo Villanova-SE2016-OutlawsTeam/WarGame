f6dbc77e6f5808c77a6ff479492ef8831a326e15
Andrew Grace <apenngrace@gmail.com>
2016-09-25 20:51:55 -0400

    Changed name of PlayerInfo to GameConstants because I added a string constant for battlefield1 and battfield2.  I replaced literal strings with this constant in warmgame.html. apg.

--- Deck.js
+++ Deck.js
@@ -137,18 +137,23 @@ var Deck = function() {     //Deck objects - an array of Card objects
     };
 }
 
-// General player information
-var PlayerInfo = function() {
+//Constants so we don't use strings directly,
+//and we'll get an error if we mistype this (which is good).
+var GameConstants = function() {
     var HUMAN = "human"
     var COMPUTER = "computer"
     var PLAYER1 = "player1"
     var PLAYER2 = "player2"
+    var BATTLEFIELD1 = "battlefield1"
+    var BATTLEFIELD2 = "battlefield2"
     
     return {
         HUMAN: HUMAN,
         COMPUTER: COMPUTER,
         PLAYER1: PLAYER1,
-        PLAYER2: PLAYER2
+        PLAYER2: PLAYER2,
+        BATTLEFIELD1: BATTLEFIELD1,
+        BATTLEFIELD2: BATTLEFIELD2
     }
 }();
 
