47bc36f187d4d8e81d2a27aaadf7fecaa66c1498
Andrew Grace <apenngrace@gmail.com>
2016-10-01 11:22:25 -0400

    Added code to evaluate the winner of a round, using the GameRound object.  Winner and Loser's cards are added to the winner's discarded_deck.  Ties are detected, but nothing done about them yet.

--- Deck.js
+++ Deck.js
@@ -196,27 +196,47 @@ var GameRound = function(startingTurn, player1, player2) {
     var p2 = player2;
 
     var isNewRound = false;
+    var winner = null;      //winner of a round
+
+    function resetPlayerTurns() {
+        p1.playedTurn = false;
+        p2.playedTurn = false;
+    }
+
+    function bothPlayedCard() {
+        return (p1.playedTurn && p2.playedTurn);
+    }
+
+    function isTie() {
+        return (p1.activeCard.value) == (p2.activeCard.value);
+    }
 
     function nextTurn() {
         currentTurn = (currentTurn == GameConstants.PLAYER1) ? 
                     GameConstants.PLAYER2 : GameConstants.PLAYER1;
 
-        getCurrentPlayer().playedTurn = true;
-
-        if (p1.playedTurn && p2.playedTurn) {
+        if (bothPlayedCard()) {
             this.isNewRound = true;    
             resetPlayerTurns();
             roundCount++;
         } else {
             this.isNewRound = false;    
         }       
-
     }
 
-    function resetPlayerTurns() {
-        p1.playedTurn = false;
-        p2.playedTurn = false;
+    //if somebody won, then return pointer to player object
+    //if no winner, return null
+    function whoWon() {
+        if (bothPlayedCard() && !isTie()) {
+            winner = (p1.activeCard.value > p2.activeCard.value) ? p1 : p2;
+            return winner;
+        }
+        return null;
     }
+
+    function whoLost() {
+        return (winner === p1) ? p2 : p1;
+    }    
 
     function whoseTurn() {
         return currentTurn;
@@ -245,7 +265,11 @@ var GameRound = function(startingTurn, player1, player2) {
         getOtherPlayer: getOtherPlayer,
         count: count,
         isNewRound: isNewRound,
-        print: print
+        print: print,
+        bothPlayedCard: bothPlayedCard,
+        isTie: isTie,
+        whoWon: whoWon,
+        whoLost: whoLost
     }
 }
 
