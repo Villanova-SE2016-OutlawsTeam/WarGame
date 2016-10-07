79fd46df2c79655752ac123730f88efba5be7796
Henri Hila <henri.hila@gmail.com>
2016-10-07 10:58:07 -0400

    created GameConstants.js

---
+++ GameConstants.js
@@ -0,0 +1,21 @@
+//Constants so we don't use strings directly,
+//and we'll get an error if we mistype this (which is good).
+var GameConstants = function() {
+    var HUMAN = "human"
+    var COMPUTER = "computer"
+    var PLAYER1 = "player1"
+    var PLAYER2 = "player2"
+    var BATTLEFIELD1 = "battlefield1"
+    var BATTLEFIELD2 = "battlefield2"
+    var PLAYERMSG = "playerMessage"
+    
+    return {
+        HUMAN: HUMAN,
+        COMPUTER: COMPUTER,
+        PLAYER1: PLAYER1,
+        PLAYER2: PLAYER2,
+        BATTLEFIELD1: BATTLEFIELD1,
+        BATTLEFIELD2: BATTLEFIELD2,
+        PLAYERMSG: PLAYERMSG
+    }
+}();
